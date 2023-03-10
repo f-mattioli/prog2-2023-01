cc9dc4bd33516d1d9d55c5d10b0eb9765e933b00
Fernando Mattioli <fernando.mattioli@facthus.edu.br>
2023-03-10 19:39:07 -0300

    Commit inicial

---
+++ exercicios.js
@@ -0,0 +1,14 @@
+/*
+ * Exercício 01
+ */
+function ex01_dobro() {
+    var n = document.getElementById('n').value;
+    document.getElementById('saida').innerHTML = 
+        'Dobro: ' + (2 * n);
+}
+
+function ex01_triplo() {
+    var n = document.getElementById('n').value;
+    document.getElementById('saida').innerHTML = 
+        'Triplo: ' + (3 * n);
+}
